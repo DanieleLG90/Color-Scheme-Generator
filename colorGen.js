1506cea6f0cafeba08c69fd7b9fede31a128d511
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2024-08-13 17:59:11 +0200

    figuring out
    understandig how to use the api.
    big step

--- colorGen.js
+++ colorGen.js
@@ -1,6 +1,21 @@
 
 
-
-fetch("https://www.thecolorapi.com/id")
+const colorPicker = document.getElementById('color-picker')
+const btnGenerateColor = document.getElementById('btn-generate-color')
+const colorDisplay = document.getElementById('color-display')
+/*
+fetch("https://www.thecolorapi.com/scheme?hex=0047AB&mode=analogic&count=5")
 .then(res => res.json())
 .then(data => console.log(data))
+*/
+
+function colorPicked(){
+    let color = colorPicker.value
+    return color
+    //console.log(color)
+}
+
+btnGenerateColor.addEventListener('click', function(){
+    colorPicked()
+    colorDisplay.style.backgroundColor = colorPicked()
+})
