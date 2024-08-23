0eef77c0fe300438b53dc228b7fc00240dedc54c
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2024-08-23 08:29:05 +0200

    css

--- colorGen.js
+++ colorGen.js
@@ -15,17 +15,17 @@ fetch(api)
 function colorPicked(){
     let color = colorPicker.value
     const cleanColor= color.substring(1)
-    
+    return cleanColor
 }
 
 btnGenerateColor.addEventListener('click', function(){
     let api = `https://www.thecolorapi.com/scheme?hex=${colorPicked()}&mode=${colorScheme.value}&count=5`
     
-    
     fetch(api)
     .then(res => res.json())
     .then(data =>{
-        console.log(data.colors)
+        console.log(data)
+        //console.log(data.colors)
         let colorArr = data.colors
         let colorVal = ''
         colorArr.forEach(function(color){
@@ -34,10 +34,8 @@ btnGenerateColor.addEventListener('click', function(){
                     <div class="colorBack" style="background-color:${color.hex.value} ;"></div>
                     <h2 class="colorHex">${color.hex.value}</h2>
                 </div>`
-            
-            console.log(color.hex.value)
-            console.log(colorVal)
         })
+
         colorDisplay.innerHTML = colorVal
     })
     
